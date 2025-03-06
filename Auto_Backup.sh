c986a1996fc6ade80962aba9ba52e255380ba370
BurhanMiyaji12 <148639320+BurhanMiyaji12@users.noreply.github.com>
2025-03-06 10:59:53 -0500

    Add files via upload

---
+++ Auto_Backup.sh
@@ -0,0 +1,15 @@
+BACKUP="/backup"
+TIME=$(date+"%Y%m%d_%H%M%S" )
+BACKUP_FILE="$BACKUP/etc_backup_$TIME. tar.gz"
+
+if [ ! -d "$BACKUP" ]; then
+	echo "Creating a backup Directory at $BACKUP"
+	mkdir -p "BACKUP"
+fi
+echo "updating system packages"
+sudo apt update && sudo apt upgrade -y
+
+echo "Backing up /etc dir to $BACKUP"
+sudo tar -czvf "$BAKCUP" /etc
+
+echo "System update and backup completed"
